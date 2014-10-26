24adb9e8449dabb6251abe7f58ad163a8523118c
raywritescode <raywritescode@users.noreply.github.com>
2014-10-25 20:21:35 -0700

    illegalIdentifiers.java - Added. Tests javac's response to illegal identifiers.

---
+++ 1Z0-803_prep/ch01/illegalIdentifiers.java
@@ -0,0 +1,16 @@
+// illegalIdentifiers.java
+//
+// Code to see what errors the Java compiler displays
+// when it encounters illegal identifiers
+//
+// Ray Santos
+// October 25, 2014
+
+public class illegalIdentifiers
+{
+   int 3personTeamName;
+   int #inPounds;
+   double -finalAmount;
+   double total_of_purchase&tax;
+   String class;
+}
