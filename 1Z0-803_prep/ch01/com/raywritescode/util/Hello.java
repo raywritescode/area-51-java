640a0b032b4f95afeb9bcee4988ae0f5ad91c82e
raywritescode <raywritescode@users.noreply.github.com>
2014-11-03 22:48:18 -0800

    Hello.java - Added

---
+++ 1Z0-803_prep/ch01/com/raywritescode/util/Hello.java
@@ -0,0 +1,11 @@
+// Hello.java
+
+package com.raywritescode.util;
+
+public class Hello
+{
+   public void printHello()
+   {
+      System.out.println( "Hello from com.raywritescode.util.Hello" );
+   }
+}
