5df21eb51a1d85d5fd20aa6eec00481619710847
raywritescode <raywritescode@users.noreply.github.com>
2014-11-03 23:54:32 -0800

    ImportTest4.java - Added

---
+++ 1Z0-803_prep/ch01/ImportTest4.java
@@ -0,0 +1,13 @@
+// ImportTest4.java
+import com.raywritescode.util.ArrayList;
+
+public class ImportTest4
+{
+   public static void main( String[] args )
+   {
+      ArrayList<String> myList = new ArrayList<String>();
+
+      java.util.ArrayList<String> myOtherList =
+         new java.util.ArrayList<String>();
+   }
+}
