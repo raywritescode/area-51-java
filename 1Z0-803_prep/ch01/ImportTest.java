f0eec69e793c5648175aa26064361048ac794cba
raywritescode <raywritescode@users.noreply.github.com>
2014-11-03 22:46:46 -0800

    ImportTest.java - Added

---
+++ 1Z0-803_prep/ch01/ImportTest.java
@@ -0,0 +1,11 @@
+// ImportTest.java
+
+public class ImportTest
+{
+   public static void main( String[] args )
+   {
+      com.raywritescode.util.Hello myHello = 
+         new com.raywritescode.util.Hello();
+      myHello.printHello();
+   }
+} 
