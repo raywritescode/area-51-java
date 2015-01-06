72dd75665172202e5961784832406a02cf17224d
raywritescode <raywritescode@users.noreply.github.com>
2015-01-05 23:08:54 -0800

    Wombat.java - Added missing semi-colon in line 10.

--- oracle_oca_jse7/ch01/Wombat.java
+++ oracle_oca_jse7/ch01/Wombat.java
@@ -7,7 +7,7 @@
 class Wombat implements Runnable {
   private int i;
   public synchronized void run() {
-    if (i%5 != 0) { i++ }
+    if (i%5 != 0) { i++; }
     for(int x=0; x<5; x++, i++)
       { if (x > 1) Thread.yield(); }
     System.out.print(i + " ");
