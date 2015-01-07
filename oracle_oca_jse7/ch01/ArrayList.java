b34fa4612f1667e5c510f5f206c4cac493e4dd3b
raywritescode <raywritescode@users.noreply.github.com>
2015-01-06 22:27:46 -0800

    ArrayList.java - Added. To test behavior of Java compiler when used simultaneously with the official Java ArrayList class.

---
+++ oracle_oca_jse7/ch01/ArrayList.java
@@ -0,0 +1,12 @@
+// ArrayList.java
+// Intentionally named ArrayList class to experiment using this class
+// simultaneously with the official Java ArrayList class.
+//
+// January 6, 2015
+// Ray Santos
+
+public class ArrayList {
+   public static void main( String[] args ) {
+      System.out.println( "fake ArrayList class" );
+   }
+}
