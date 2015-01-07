f9fbeb6d1edc081f44091e11d512fbbee56f6616
raywritescode <raywritescode@users.noreply.github.com>
2015-01-06 22:16:57 -0800

    SpecialMain02.java - Added. Uses alternative declaration for 'special' main().

---
+++ oracle_oca_jse7/ch01/SpecialMain02.java
@@ -0,0 +1,12 @@
+// SpecialMain02.java
+// Experimenting creating a class that uses an alternative form
+// of the declaration for a legal "special" main().
+//
+// January 6, 2015
+// Ray Santos
+
+public class SpecialMain02 {
+   public static void main( String... x ) {
+      System.out.println("This class uses \"public static void main( String... x )\"");
+   }
+}
