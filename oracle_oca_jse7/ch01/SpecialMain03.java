32d76d8b5e9b74f9829953a18b866b548d9156d6
raywritescode <raywritescode@users.noreply.github.com>
2015-01-06 22:20:47 -0800

    SpecialMain03.java - Added. Uses alternative declaration for 'special' main().

---
+++ oracle_oca_jse7/ch01/SpecialMain03.java
@@ -0,0 +1,12 @@
+// SpecialMain03.java
+// Experimenting creating a class that uses an alternative form
+// of the declaration for a legal "special" main().
+//
+// January 6, 2015
+// Ray Santos
+
+public class SpecialMain03 {
+   static public void main( String randomWord[]  ) {
+      System.out.println("This class uses \"static public void main( String randomWord[] )\"");
+   }
+}
