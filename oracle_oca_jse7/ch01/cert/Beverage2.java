f2f9a3cfcf98eb5f81fc81f8e53a6a0c4cc587ba
raywritescode <raywritescode@users.noreply.github.com>
2015-01-13 15:24:58 -0800

    Added small set of class files to examine public class access behavior.

---
+++ oracle_oca_jse7/ch01/cert/Beverage2.java
@@ -0,0 +1,7 @@
+// Beverage2.java
+// This class is accessible to classes outside the cert package
+// because the class access is public.
+
+package cert;
+
+public class Beverage2 { }
---
+++ oracle_oca_jse7/ch01/exam/stuff/Tea2.java
@@ -0,0 +1,8 @@
+// Tea2.java
+// javac compiles this class because the class modifier for Beverage2 is public.
+
+package exam.stuff;
+
+import cert.Beverage2;
+
+class Tea2 extends Beverage2 {}
