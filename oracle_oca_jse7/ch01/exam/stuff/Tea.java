ed4778258575cae1f35b3eae4163a781ca8499d3
raywritescode <raywritescode@users.noreply.github.com>
2015-01-13 15:22:48 -0800

    exam/stuff/Tea.java - Added comment examples of javac error output.

--- oracle_oca_jse7/ch01/exam/stuff/Tea.java
+++ oracle_oca_jse7/ch01/exam/stuff/Tea.java
@@ -1,7 +1,22 @@
 // Tea.java
+// javac cannot compile this class because the class modifier for
+// cert.Beverage is default access.
 
 package exam.stuff;
 
 import cert.Beverage;
 
 class Tea extends Beverage {}
+
+// javac compile error produced
+//
+//   $ javac exam/stuff/Tea.java
+//   exam/stuff/Tea.java:5: error: Beverage is not public in cert; cannot be accessed from outside package
+//   import cert.Beverage;
+//             ^
+//   exam/stuff/Tea.java:7: error: cannot find symbol
+//   class Tea extends Beverage {}
+//                  ^
+//   symbol: class Beverage
+//   2 errors
+
