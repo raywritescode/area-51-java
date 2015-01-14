439779ecab5149c837c74645e642eeaeb0995979
raywritescode <raywritescode@users.noreply.github.com>
2015-01-13 16:34:29 -0800

    Added - class files for Exercise 1-1, page 23.

---
+++ oracle_oca_jse7/ch01/Apple.java
@@ -0,0 +1,6 @@
+// Apple.java
+// Class attempts to extend class food.Fruit
+
+import food.Fruit;
+
+class Apple extends Fruit { /* comment block */ }
---
+++ oracle_oca_jse7/ch01/food/Fruit.java
@@ -0,0 +1,6 @@
+// Fruit.java
+// Exercise 1-1 Page 23
+
+package food;
+
+public abstract class Fruit { /* comment block */ }
