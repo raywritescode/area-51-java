873a76037ef27b04e7e4ae2465928d99f0b0d110
raywritescode <raywritescode@users.noreply.github.com>
2015-01-14 13:46:34 -0800

    Added - class files to examine public class member inheritance from an outside package.

---
+++ oracle_oca_jse7/ch01/publicMembers/cert/Roo.java
@@ -0,0 +1,9 @@
+// Roo.java
+
+package cert;
+
+public class Roo {
+   public String dooRooThings() {
+      return "This message returned from dooRooThings()";
+   }
+}
---
+++ oracle_oca_jse7/ch01/publicMembers/notcert/Cloo.java
@@ -0,0 +1,16 @@
+// Cloo.java
+
+package notcert;
+
+import cert.Roo;
+
+class Cloo extends Roo {
+  public void testCloo() {
+    System.out.println(dooRooThings());
+  }
+
+  public static void main(String[] args) {
+    Cloo cloo = new Cloo();
+    cloo.testCloo();
+  }
+}
