07d54b23db51f5f7377a22cdb1fee6cce8d5306f
raywritescode <raywritescode@users.noreply.github.com>
2015-01-16 22:34:59 -0800

    Added - cert/ClooDoRooThings.java. Subclass can use an method name identical to a private method in its superclass.

---
+++ oracle_oca_jse7/ch01/privateMembers/cert/ClooDoRooThings.java
@@ -0,0 +1,23 @@
+// ClooDoRooThings.java
+// This subclass of Roo can't use the private Roo.doRooThings() method
+// so it uses it's own dooRooThings() method, which is NOT and overriding
+// of Roo.doRooThings()
+
+package cert;
+
+class ClooDoRooThings extends Roo {
+  private String doRooThings() {
+    return "This message is from ClooDoRooThings.doRooThings()"; 
+  }
+
+  public static void main(String[] args) {
+    ClooDoRooThings cloo = new ClooDoRooThings();
+    System.out.println(cloo.doRooThings());
+  }
+}
+
+// application's output
+
+//  $ java cert.ClooDoRooThings 
+//  This message is from ClooDoRooThings.doRooThings()
+
