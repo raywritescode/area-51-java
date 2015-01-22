cb77e9d560e2f75cd3f715269a85653029fd4ce6
raywritescode <raywritescode@users.noreply.github.com>
2015-01-22 00:02:32 -0800

    FinalArguments.java - Updated to include comments describing error location and javac error displayed.

--- oracle_oca_jse7/ch01/finalArguments/FinalArguments.java
+++ oracle_oca_jse7/ch01/finalArguments/FinalArguments.java
@@ -2,11 +2,11 @@
 
 public class FinalArguments {
 
-   private void printTwoIntegers( int x, int y ) {
+   private void printTwoIntegers( int x, final int y ) {
       System.out.println( "First integer entered is " + x );     
       System.out.println( "Second integer entered is " + y );
       x++;
-      y++;
+      y++; // javac will fail here. Not allowed to modify argument value marked final
       System.out.println( "First integer entered is " + x );     
       System.out.println( "Second integer entered is " + y );
    }
@@ -16,3 +16,11 @@ public class FinalArguments {
       foo.printTwoIntegers( 10, 20 );
    }
 }
+
+// javac error...
+
+//   $javac FinalArguments.java 
+//   FinalArguments.java:9: error: final parameter y may not be assigned
+//         y++;
+//         ^
+//   1 error
