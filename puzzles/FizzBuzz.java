46021c51e3641d42fdc54609ccafc07c9120c18d
raywritescode <raywritescode@users.noreply.github.com>
2015-01-22 23:27:48 -0800

    Updated - FizzBuzz.java - Added Java code to support pseudocode.

--- puzzles/FizzBuzz.java
+++ puzzles/FizzBuzz.java
@@ -5,11 +5,29 @@
 // "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers
 // which are mulitples of both three and five print "FizzBuzz".
 
-/* pseudocode
+class FizzBuzz {
+   public static void main( String[] args ) {
    
-   Repeat starting at 1 to 100
-      If the number is divisible by 3 and 5 print "FizzBuzz"
-      Otherwise, if the number is divisible by 3 print "Fizz"
-      Otherwise, if the number is divisible by 5 print "Buzz"
-      increment the count by 1
-*/
+//   Repeat starting at 1 to 100
+     for ( int i = 1; i <= 100; i++ ) {
+
+//      If the number is divisible by 3 and 5 print "FizzBuzz"
+        if ( ( i % 3 == 0 ) && ( i % 5 == 0 ) )
+           System.out.println( "FizzBuzz" );
+       
+//      Otherwise, if the number is divisible by 3 print "Fizz"
+        else if ( i % 3 == 0 )
+           System.out.println( "Fizz" );
+
+//      Otherwise, if the number is divisible by 5 print "Buzz"
+        else if ( i % 5 == 0 )
+           System.out.println( "Buzz" );
+
+//      Otherwise, print the number
+        else System.out.println( i );
+
+//      increment the count by 1
+        // handled by the for loop
+      }
+   }
+}
