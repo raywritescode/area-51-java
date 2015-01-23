4e9ca3d5a88293f2e345b1b63d2eb8b5b778c07d
raywritescode <raywritescode@users.noreply.github.com>
2015-01-22 23:11:27 -0800

    Added - FizzBuzz.java - pseudocode

---
+++ puzzles/FizzBuzz.java
@@ -0,0 +1,15 @@
+// FizzBuzz.java
+// The classic FizzBuzz coding test.
+
+// Write a program that prints the numbers from 1 to 100. But for multiples of three print
+// "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers
+// which are mulitples of both three and five print "FizzBuzz".
+
+/* pseudocode
+   
+   Repeat starting at 1 to 100
+      If the number is divisible by 3 and 5 print "FizzBuzz"
+      Otherwise, if the number is divisible by 3 print "Fizz"
+      Otherwise, if the number is divisible by 5 print "Buzz"
+      increment the count by 1
+*/
