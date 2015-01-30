c55a634ad83277688097312f16822a0158006164
raywritescode <raywritescode@users.noreply.github.com>
2015-01-29 21:41:06 -0800

    Added - Foo.java - Simple code example of constructor declaration.

---
+++ oracle_oca_jse7/ch01/constructorDeclarations/Foo.java
@@ -0,0 +1,7 @@
+// Foo.java
+// Simple code examples of legal constructors
+
+class Foo {
+   protected Foo() { }        // this is Foo's constructor
+   protected void Foo() { }   // this is a badly named, but legal, method
+}
