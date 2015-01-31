8f1f13c47585571b743540c52be926497534baf2
raywritescode <raywritescode@users.noreply.github.com>
2015-01-30 23:51:49 -0800

    Added - Employee.java - simple instance variables example.

---
+++ oracle_oca_jse7/ch01/instanceVariables/Employee.java
@@ -0,0 +1,10 @@
+// Employee.java
+// Instance variables are defined inside the class, but outside
+// of any method, and are initialized only when the class
+// is instantiated.
+
+class Employee {
+   private String name;
+   private String title;
+   private String manager;
+}
