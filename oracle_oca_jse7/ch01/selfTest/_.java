0ed9fccf61f896c8c68c1c05f90ffebf30eb4ac3
raywritescode <raywritescode@users.noreply.github.com>
2015-02-10 16:18:11 -0800

    Added - _.java - Ch01 Self Test 3.

---
+++ oracle_oca_jse7/ch01/selfTest/_.java
@@ -0,0 +1,13 @@
+// _.java
+
+import static java.lang.System.*;
+
+class _ {
+   static public void main( String[] __A_V_) {
+      String $ = "";
+      out.println( __A_V_.length );
+      for ( int x = 0; ++x < __A_V_.length; )  // for loop
+         $ += __A_V_[ x ];
+      out.println( $ );
+   }
+}
